4bb96c63c67cd8f98440cf0a92372eeca226a605
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-07-05 07:38:12 -0300

    Create server restify

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("testando ")
+import * as restify from "restify";
+
+const server = restify.createServer({
+    name: "Meat api",
+    version: "1.0.0"
+});
+
+server.use(restify.plugins.queryParser());
+
+server.get("/info", (request, response, next) => {
+    response.json({ 
+        method: request.method,
+        path: request.path(),
+        userAgent: request.userAgent(),
+        querystring: request.query
+    });
+    return next();
+});
+
+server.listen(3000, () => console.log(" Server running in http://localhost:3000"));
